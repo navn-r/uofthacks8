f34d353e297d6bf33a904d2153511e02eb8ae1e2
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-02-20 02:50:19 -0500

    mock followers

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import {
+  IonAvatar,
   IonContent,
   IonHeader,
   IonPage,
@@ -7,16 +8,35 @@ import {
 } from "@ionic/react";
 import "./HomePage.css";
 
+const MockFollowers = [
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7742?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7743?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7744?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7745?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7746?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7747?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7748?d=identicon&f=y',
+  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7749?d=identicon&f=y',
+];
+
 const HomePage: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle class="ion-text-center">Home</IonTitle>
+          <IonTitle class="ion-text-center">Munchify</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-
+        <h4 className="home-page-followers-title">Followers:</h4>
+        <div className="followers-icon-container">
+          {MockFollowers.map((f, i) => (
+            <IonAvatar key={i}>
+              <img src={f} />
+            </IonAvatar>
+          ))}
+        </div>
       </IonContent>
     </IonPage>
   );
