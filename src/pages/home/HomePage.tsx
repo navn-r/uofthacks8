a963282b9dd252528a5da29e12abfd63de7a5ba1
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-02-20 03:20:58 -0500

    added followers useEfefect

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -6,21 +6,44 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { useEffect, useState } from "react";
+import { useData } from "../../components/Data/DataContext";
+import { getFollowers } from "../../firebase/api";
+import { Recipe, User } from "../../firebase/models";
 import "./HomePage.css";
 
 const MockFollowers = [
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7742?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7743?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7744?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7745?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7746?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7747?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7748?d=identicon&f=y',
-  'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7749?d=identicon&f=y',
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7742?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7743?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7744?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7745?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7746?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7747?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7748?d=identicon&f=y",
+  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7749?d=identicon&f=y",
 ];
 
 const HomePage: React.FC = () => {
+  const { user, loading } = useData();
+  const [followers, setFollowers] = useState([] as any[]);
+  const [recipes, setRecipes] = useState([] as Recipe[]);
+
+  useEffect(() => {
+    if(loading) return;
+    const unsubscribe = async () => {
+      const followers = await getFollowers(user.followerIds);
+      setFollowers(followers.map((f: User) => {
+          return {
+            id: f.id,
+            photoURL: f.photoURL,
+          };
+        })
+      );
+    };
+    unsubscribe();
+  }, [user, loading]);
+
   return (
     <IonPage>
       <IonHeader>
@@ -31,9 +54,9 @@ const HomePage: React.FC = () => {
       <IonContent fullscreen>
         <h4 className="home-page-followers-title">Followers:</h4>
         <div className="followers-icon-container">
-          {MockFollowers.map((f, i) => (
-            <IonAvatar key={i}>
-              <img src={f} />
+          {followers.map((f: any) => (
+            <IonAvatar key={f.id}>
+              <img src={f.photoURL} />
             </IonAvatar>
           ))}
         </div>
