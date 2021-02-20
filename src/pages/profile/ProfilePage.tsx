0e02c39aa1666badb963f3e37f5ddf17e2164a84
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-02-20 02:30:08 -0500

    fix color scheme

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -39,11 +39,6 @@ const ProfilePage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <div className="user-info">
           <IonAvatar>
             <img src={authUser.photoURL} />
