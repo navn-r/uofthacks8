49615a98c198e5a1ee2c0dc4222927da1bc96393
Your name here <ilancito70@gmail.com>
2021-02-20 17:04:12 -0500

    fixed display bug

--- src/pages/profile_visitor/ProfileVisitor.tsx
+++ src/pages/profile_visitor/ProfileVisitor.tsx
@@ -38,7 +38,7 @@ const ProfilePage: React.FC<ProfilePageProps> = ({
       setRecipes(await getRecipes(user.recipeIds));
     };
     unsubscribe();
-  }, [userId]);
+  }, [profileUser, userId]);
 
   return (
     <IonModal isOpen={showModal} backdropDismiss={false}>
