c038591d1ce1495b26b63ed67b2e335148d26c39
SomeStupidIdiot <james.yung.lee@gmail.com>
2021-02-20 13:18:38 -0500

    recipe search

--- src/pages/recipe_search/RecipeSearch.tsx
+++ src/pages/recipe_search/RecipeSearch.tsx
@@ -1,30 +1,21 @@
 import {
   IonButton,
   IonCheckbox,
+  IonContent,
   IonIcon,
-  IonImg,
-  IonInput,
   IonItem,
   IonLabel,
   IonList,
+  IonPage,
   IonRange,
   IonSearchbar,
-  IonTextarea,
 } from "@ionic/react";
 import { close } from "ionicons/icons";
-import React, { useEffect } from "react";
+import React from "react";
 import { foods, tags } from "../../firebase/constants";
 import "./RecipeSearch.css";
-import { makeRecipe } from "../../firebase/api";
-interface RecipeUploadProps {
-  onSuccess: () => any;
-  showModal: boolean;
-}
 
-const RecipeSearch: React.FC<RecipeUploadProps> = ({
-  onSuccess,
-  showModal,
-}) => {
+const RecipeSearch: React.FC = () => {
   const [search, setSearch] = React.useState("");
   const [foodItems, setFoodItems] = React.useState<string[]>([]);
   const [ingredients, setIngredients] = React.useState<string[]>([]);
@@ -33,112 +24,117 @@ const RecipeSearch: React.FC<RecipeUploadProps> = ({
   );
 
   return (
-    <div className="recipe-upload-container">
-      <div>
-        <IonSearchbar
-          value={search}
-          placeholder="Search for ingredients"
-          onIonChange={(e) => {
-            const input = e.detail.value!.trim().toLowerCase();
-            setSearch(e.detail.value!);
-            if (!input || input.length < 3) {
-              setFoodItems([]);
-              return;
-            }
-            const items = foods
-              .filter((food) => food.startsWith(input))
-              .slice(0, 10);
-            if (items !== foodItems) setFoodItems(items);
-          }}
-          showCancelButton="never"
-        />
-        <div className="food-items">
-          {foodItems.length <= 10 &&
-            foodItems.map((item) => {
-              return (
-                <div
-                  key={item}
-                  className="food-item-tag"
-                  onClick={() => {
-                    if (!ingredients.includes(item))
-                      setIngredients([...ingredients, item]);
-                  }}
-                >
-                  {item}
-                </div>
-              );
-            })}
-        </div>
-        <IonList>
-          {!!ingredients.length && (
-            <div className="ingredient-titles-container">
-              <p>Ingredient</p>
-              <p>amount</p>
-              <p>measure unit</p>
+    <IonPage>
+      <IonContent fullscreen>
+        <div className="recipe-upload-container">
+          <div>
+            <IonSearchbar
+              value={search}
+              placeholder="Search for ingredients"
+              onIonChange={(e) => {
+                const input = e.detail.value!.trim().toLowerCase();
+                setSearch(e.detail.value!);
+                if (!input || input.length < 3) {
+                  setFoodItems([]);
+                  return;
+                }
+                const items = foods
+                  .filter((food) => food.startsWith(input))
+                  .slice(0, 10);
+                if (items !== foodItems) setFoodItems(items);
+              }}
+              showCancelButton="never"
+            />
+            <div className="food-items">
+              {foodItems.length <= 10 &&
+                foodItems.map((item) => {
+                  return (
+                    <div
+                      key={item}
+                      className="food-item-tag"
+                      onClick={() => {
+                        if (!ingredients.includes(item))
+                          setIngredients([...ingredients, item]);
+                      }}
+                    >
+                      {item}
+                    </div>
+                  );
+                })}
             </div>
-          )}
-          <div className="ingredient-list">
-            {ingredients.map((item, index) => (
-              <div className="ingredient-item-row" key={item}>
-                <IonLabel>{item}</IonLabel>
-                <IonButton
-                  fill="clear"
-                  color="danger"
-                  onClick={() => {
-                    setIngredients(ingredients.filter((_, i) => i !== index));
-                  }}
-                  size="small"
-                >
-                  <IonIcon slot="icon-only" icon={close}></IonIcon>
-                </IonButton>
+            <IonList>
+              {!!ingredients.length && (
+                <div className="ingredient-titles-container">
+                  <p>Ingredient</p>
+                  <p>amount</p>
+                  <p>measure unit</p>
+                </div>
+              )}
+              <div className="ingredient-list">
+                {ingredients.map((item, index) => (
+                  <div className="ingredient-item-row" key={item}>
+                    <IonLabel>{item}</IonLabel>
+                    <IonButton
+                      fill="clear"
+                      color="danger"
+                      onClick={() => {
+                        setIngredients(
+                          ingredients.filter((_, i) => i !== index)
+                        );
+                      }}
+                      size="small"
+                    >
+                      <IonIcon slot="icon-only" icon={close}></IonIcon>
+                    </IonButton>
+                  </div>
+                ))}
               </div>
-            ))}
+            </IonList>
           </div>
-        </IonList>
-      </div>
-      <div>
-        <IonList>
-          <h4 className="tags-title">Tags</h4>
-          <div className="taglist">
-            {tags.map((item, index) => {
-              return (
-                <IonItem key={item}>
-                  <IonLabel>{item}</IonLabel>
-                  <IonCheckbox
-                    onClick={() => {
-                      const cpy = [...tagItems];
-                      cpy[index] = !cpy[index];
-                      setTagItems(cpy);
-                    }}
-                  />
-                </IonItem>
-              );
-            })}
+          <div>
+            <IonList>
+              <h4 className="tags-title">Tags</h4>
+              <div className="taglist">
+                {tags.map((item, index) => {
+                  return (
+                    <IonItem key={item}>
+                      <IonLabel>{item}</IonLabel>
+                      <IonCheckbox
+                        onClick={() => {
+                          const cpy = [...tagItems];
+                          cpy[index] = !cpy[index];
+                          setTagItems(cpy);
+                        }}
+                      />
+                    </IonItem>
+                  );
+                })}
+              </div>
+            </IonList>
           </div>
-        </IonList>
-      </div>
-      <div>
-        <h4 className="cost-title">Cost</h4>
-        <IonItem>
-          <IonRange
-            dualKnobs={true}
-            min={1}
-            max={4}
-            step={1}
-            snaps={true}
-            color="secondary"
-            onIonChange={(e) => {
-              console.log(e.detail.value);
-            }}
-          >
-            <IonLabel slot="start">$</IonLabel>
-            <IonLabel slot="end">$$$$</IonLabel>
-          </IonRange>
-        </IonItem>
-      </div>
-
-      <IonButton color="success">Find Recipes</IonButton>
-    </div>
+          <div>
+            <h4 className="cost-title">Cost</h4>
+            <IonItem>
+              <IonRange
+                dualKnobs={true}
+                min={1}
+                max={4}
+                step={1}
+                snaps={true}
+                color="secondary"
+                onIonChange={(e) => {
+                  console.log(e.detail.value);
+                }}
+              >
+                <IonLabel slot="start">$</IonLabel>
+                <IonLabel slot="end">$$$$</IonLabel>
+              </IonRange>
+            </IonItem>
+          </div>
+          <IonButton color="success">Find Recipes</IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
 
