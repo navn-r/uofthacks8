6cfbc2b75e95e0aa8994983ff1b1470b0e7e9888
Your name here <ilancito70@gmail.com>
2021-02-20 16:14:41 -0500

    pages

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -9,22 +9,10 @@ import {
 } from "@ionic/react";
 import { useEffect, useState } from "react";
 import { useData } from "../../components/Data/DataContext";
-import { getAllRecipes, getFollowers, getRecipes } from "../../firebase/api";
+import { getAllRecipes, getFollowed, getRecipes } from "../../firebase/api";
 import { Recipe, User } from "../../firebase/models";
 import "./HomePage.css";
 import RecipeCard from "../../components/recipe/RecipeCard";
-
-const MockFollowers = [
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7742?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7743?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7744?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7745?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7746?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7747?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7748?d=identicon&f=y",
-  "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7749?d=identicon&f=y",
-];
 
 const HomePage: React.FC = () => {
   const { user, loading } = useData();
@@ -34,7 +22,7 @@ const HomePage: React.FC = () => {
   useEffect(() => {
     if (loading) return;
     const unsubscribe = async () => {
-      const followers = await getFollowers(user.followerIds);
+      const followers = await getFollowed(user.followingIds);
       const recipes = await getAllRecipes();
       setRecipes(recipes);
       setFollowers(
@@ -56,10 +44,11 @@ const HomePage: React.FC = () => {
           <IonTitle color="primary" class="ion-text-west">Munchify</IonTitle>
         </IonToolbar>
       </IonHeader>
+      
       <IonContent fullscreen>
         <h4 className="home-page-followers-title">Followers:</h4>
         <div className="followers-icon-container">
-          {(!!followers.length ? followers : MockFollowers).map((f, i) => (
+          {followers.map((f, i) => (
             <IonAvatar key={i}>
               <img src={f.photoURL ?? f} />
             </IonAvatar>
