f29e178fc8d5825739e3cbd217dc7d72693f3ae5
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-02-19 21:37:06 -0500

    firebase done

---
+++ src/firebase/config.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyBEwNpTo1y27ZGuahae0YIaC3t-uY9fRj0",
+  authDomain: "uofthacks2021.firebaseapp.com",
+  projectId: "uofthacks2021",
+  storageBucket: "uofthacks2021.appspot.com",
+  messagingSenderId: "205136705715",
+  appId: "1:205136705715:web:f951a5ec99cb950c2a98c6"
+});
+
+export const auth = app.auth();
+export const db = app.firestore();
+export default app;
