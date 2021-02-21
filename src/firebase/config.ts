ac8059b533cef4839a0fe8ecbafef056f9028f7b
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-02-21 04:05:10 -0500

    migrated back to db

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,18 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import 'firebase/firestore';
 
-
-const TEMP = {
-  apiKey: "AIzaSyBNQ_pyoZMquEJo-zQOx91O0XN7gR80oQc",
-  authDomain: "munchify-temp.firebaseapp.com",
-  projectId: "munchify-temp",
-  storageBucket: "munchify-temp.appspot.com",
-  messagingSenderId: "169854324606",
-  appId: "1:169854324606:web:e9fa5afd0e77efaca7ed2f"
-};
-
-
-const OLD = {
+const CONFIG = {
   apiKey: "AIzaSyBEwNpTo1y27ZGuahae0YIaC3t-uY9fRj0",
   authDomain: "uofthacks2021.firebaseapp.com",
   projectId: "uofthacks2021",
@@ -22,7 +11,7 @@ const OLD = {
   appId: "1:205136705715:web:f951a5ec99cb950c2a98c6"
 }
 
-const app = firebase.initializeApp(TEMP);
+const app = firebase.initializeApp(CONFIG);
 
 export const auth = app.auth();
 export const db = app.firestore();
