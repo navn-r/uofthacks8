f74b188d54faf5d49ea12173f6b62709c34705d1
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-02-23 17:43:47 -0500

    Revert "Finally Done"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import 'firebase/firestore';
 
-const CONFIG = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyBEwNpTo1y27ZGuahae0YIaC3t-uY9fRj0",
   authDomain: "uofthacks2021.firebaseapp.com",
   projectId: "uofthacks2021",
   storageBucket: "uofthacks2021.appspot.com",
   messagingSenderId: "205136705715",
   appId: "1:205136705715:web:f951a5ec99cb950c2a98c6"
-}
-
-const app = firebase.initializeApp(CONFIG);
+});
 
 export const auth = app.auth();
 export const db = app.firestore();
